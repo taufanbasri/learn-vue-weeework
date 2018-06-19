60c702175afdb4a629fc2e4d7e9ba70ef30e8574
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-06-19 11:44:36 +0700

    Methods Properties

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,11 +5,17 @@ var app = new Vue({
     show2: 'ready',
     message: 'Hello World',
     int1: 1,
-    int2: 2
+    int2: 2,
+    result: null
   },
   computed: {
     sum() {
       return this.int1 + this.int2;
+    }
+  },
+  methods: {
+    sumProcess() {
+      return this.result = this.int1 + this.int2;
     }
   }
 })
