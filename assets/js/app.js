6048b0a044ae5fd55f6c0cf453ccdcf931040604
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-06-22 06:55:46 +0700

    Watch properties

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,9 @@ var app = new Vue({
     message: 'Hello World',
     int1: 1,
     int2: 2,
-    result: null
+    result: null,
+    km: 0,
+    m: 0
   },
   computed: {
     sum() {
@@ -16,6 +18,16 @@ var app = new Vue({
   methods: {
     sumProcess(int3) {
       return this.result = this.int1 + this.int2 + int3;
+    }
+  },
+  watch: {
+    km(val) {
+      this.km = val;
+      this.m = val * 1000;
+    },
+    m(val) {
+      this.km = val / 1000;
+      this.m = val;
     }
   }
 })
