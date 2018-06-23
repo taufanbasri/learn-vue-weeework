849a1cf053c37b11a2cdba61822143872002090f
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-06-23 22:11:29 +0700

    Using properties in components

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,15 @@ Vue.component('greeting', {
   template: '<b>Hello, Vue!</b>'
 })
 
+Vue.component('button-counter', {
+  data() {
+    return {
+      count: 0
+    };
+  },
+  template: '<button @click="count++">Clicked {{ count }} times</button>'
+})
+
 var app = new Vue({
   el: '#app'
 })
