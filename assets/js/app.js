01b720487ddb0b769763eb4aa6b655861094ee8c
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-06-23 18:47:59 +0700

    v-on for key modifier

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,8 +4,20 @@ var app = new Vue({
     message: ''
   },
   methods: {
-    showAlert() {
-      alert('button has clicked')
-    }
+    escKey() {
+      this.message = 'You pressing esc key'
+    },
+    spaceKey() {
+      this.message = 'You pressing space key'
+    },
+    upKey() {
+      this.message = 'You pressing up key'
+    },
+    downKey() {
+      this.message = 'You pressing down key'
+    },
+    aKey() {
+      this.message = 'You pressing a key'
+    },
   }
 })
