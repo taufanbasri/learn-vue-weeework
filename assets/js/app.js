897eec6053e6b728c20b7378455098837139b92d
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-06-23 21:25:07 +0700

    Component

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,7 @@
+Vue.component('greeting', {
+  template: '<b>Hello, Vue!</b>'
+})
+
 var app = new Vue({
-  el: '#app',
-  data: {
-    vehicles: [
-      {type: 'Car', brand: 'Toyota', color: 'red'},
-      {type: 'Car', brand: 'Daihatsu', color: 'blue'},
-      {type: 'Motorcycle', brand: 'Ducati', color: 'green'},
-    ],
-    object: {
-      firstName: 'Taufan',
-      lastName: 'Prasetyo',
-      age: 26
-    }
-  }
+  el: '#app'
 })
