4ee836ecff587a580c9345957a27260c02cb55a0
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-06-24 20:23:02 +0700

    Passing data with props in component

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,20 @@
-Vue.component('greeting', {
-  template: '<b>Hello, Vue!</b>'
-})
-
-Vue.component('button-counter', {
-  data() {
-    return {
-      count: 0
-    };
-  },
-  template: '<button @click="count++">Clicked {{ count }} times</button>'
+Vue.component('blog-post', {
+  props: ['post'],
+  template: `
+    <div>
+      <h3>{{ post.title }}</h3>
+      <p v-text="post.content"></p>
+    </div>
+  `
 })
 
 var app = new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+    posts: [
+      {id: 1, title: 'Title 1', content: 'Content 1'},
+      {id: 2, title: 'Title 2', content: 'Content 2'},
+      {id: 3, title: 'Title 3', content: 'Content 3'},
+    ]
+  }
 })
